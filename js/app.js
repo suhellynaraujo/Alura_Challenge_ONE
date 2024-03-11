04f8a7ca2d82a93052967bb8d8245bdcad63f46a
suhellynaraujo <github.interface@gmail.com>
2024-03-10 23:12:15 -0300

    primeira tentativa de criptografia

--- js/app.js
+++ js/app.js
@@ -3,14 +3,59 @@ function TextoTela(tag, texto){
     campo.innerHTML = texto;
 
 }
+
 TextoTela('textarea', 'Digite seu texto');
 TextoTela('h4', 'Apenas letras minúsculas e sem acento.');
+TextoTela('h1', 'Nenhuma mensagem encontrada');
+TextoTela('p', 'Digite um texto que você deseja criptografar ou descriptografar.');
 
-function Criptografia(){
-   TextoTela('textarea', 'texto criptografado');
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+function Criptografia(){ 
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
 }
 
 function Descriptografia(){   
-    TextoTela('textarea', 'texto descriptografado');
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
     
+}
+
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+function btnCopiar(){
+    let textoCopiado = document.querySelector(".mensagem");
+    textoCopiado.select();
+    document.execCommand("copy");
+    textArea.setSelectionRange(0, 0);
+    mensagem.value = "";
 }
