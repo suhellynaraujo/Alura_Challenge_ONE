f5ed5e2bd6e31c404378b90811841882ec908f87
suhellynaraujo <github.interface@gmail.com>
2024-03-16 23:07:17 -0300

    ajustes finais, CSS, funcções de cript e descript

--- js/app.js
+++ js/app.js
@@ -1,68 +1,60 @@
+
 function TextoTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 
 }
 
-TextoTela('textarea', 'Digite seu texto');
+//TextoTela('textarea', 'Digite seu texto');
 TextoTela('h4', 'Apenas letras minúsculas e sem acento.');
 TextoTela('h1', 'Nenhuma mensagem encontrada');
 TextoTela('p', 'Digite um texto que você deseja criptografar ou descriptografar.');
 
-let textArea = document.querySelector('#conteudo__ladoEsquerdo__texto__digiteTexto');
-let mensagem = document.querySelector("#conteudo__ladoDireito__paragrafo");
-console.log(textArea);
-console.log(mensagem);
+let textoDeEntrada = document.querySelector('#conteudo__ladoEsquerdo__texto__TextoDeEntrada');
+let textoDeSaida = document.querySelector('#conteudo__ladoEsquerdo__texto__TextoDeSaida');
+let matrizCriptografia = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufa"]];
 
-//A letra "e" é convertida para "enter"
-//A letra "i" é convertida para "imes"
-//A letra "a" é convertida para "ai"
-//A letra "o" é convertida para "ober"
-//A letra "u" é convertida para "ufat"
-
-function Criptografia(){
-    let textoEncriptado = encriptar(textArea.value);
-    mensagem.value = textoEncriptado;
-    textArea.value = "";
-    
+function Criptografia() {
+    let resultadoCriptografado = criptografia(textoDeEntrada.value.toLowerCase());
+    textoDeSaida.value = resultadoCriptografado;
 }
 
-function encriptar(stringEncriptada) {
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase();
+function Descriptografia() {
+    let resultadoTraduzido = descriptografia(textoDeEntrada.value.toLowerCase());
+    textoDeSaida.value = resultadoTraduzido;
+}
 
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringEncriptada.includes(matrizCodigo[i][0])) {
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+function criptografia(textoParaCriptografar) {
+    for(let i = 0; i < matrizCriptografia.length; i++) {
+        if(textoParaCriptografar.includes(matrizCriptografia[i][0])) {
+            textoParaCriptografar = textoParaCriptografar.replaceAll(matrizCriptografia[i][0], matrizCriptografia[i][1])
         }
     }
-
-    return stringEncriptada;
+    if (textoParaCriptografar !== "") {
+        document.querySelector('#mensagem').style.display = 'none';
+        document.querySelector('#conteudo__ladoEsquerdo__texto__TextoDeSaida').style.display = 'block';
+        document.querySelector('#botaoCopiar').style.display = 'block';
+    }
+    return textoParaCriptografar;
 }
 
-function Descriptografia(){
-    let textoDesencriptado = desencriptar(textArea.value);
-    mensagem.value = textoDesencriptado;
-    textArea.value = "";
-}
-
-function desencriptar(stringDesencriptada) {
-    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringDesencriptada = stringDesencriptada.toLowerCase();
-
-    for (let i = 0; i < matrizCodigo.length; i++) {
-        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
-            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+function descriptografia(textoParaDescriptografar) {
+    for(let i = 0; i < matrizCriptografia.length; i++) {
+        if(textoParaDescriptografar.includes(matrizCriptografia[i][1])) {
+            textoParaDescriptografar = textoParaDescriptografar.replaceAll(matrizCriptografia[i][1], matrizCriptografia[i][0])
         }
     }
-
-    return stringDesencriptada;
+    if (textoParaDescriptografar !== "") {
+        document.querySelector('#mensagem').style.display = 'none';
+        document.querySelector('#conteudo__ladoEsquerdo__texto__TextoDeSaida').style.display = 'block';
+        document.querySelector('#botaoCopiar').style.display = 'block';
+    }
+    return textoParaDescriptografar;
 }
 
-function btnCopiar(){
-    let textoCopiado = document.querySelector(".mensagem");
+function copiar() {
+    let textoCopiado = document.querySelector('#conteudo__ladoEsquerdo__texto__TextoDeSaida');
     textoCopiado.select();
-    document.execCommand("copy");
-    textArea.setSelectionRange(0, 0);
-    mensagem.value = "";
+    textoCopiado.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(textoCopiado.value);
 }
